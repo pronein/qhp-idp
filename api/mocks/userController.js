f05c244359f63b47e5ccdcb7117a658e69100707
Adam Schrader <adam.dot.schrader@gmail.com>
2017-02-22 12:33:04 -0500

    Handle successful user registration POST [201]

--- api/mocks/userController.js
+++ api/mocks/userController.js
@@ -1,10 +1,26 @@
 'use strict';
 
 module.exports = {
-  registerUser: registerUser
+  registerUser: registerUser_mock
 };
 
-function registerUser(req, res, next) {
-  console.log('Request [swagger] Body: ' + JSON.stringify(req.swagger.body));
-  res.sendStatus(200);
+function registerUser_mock(req, res, next) {
+  console.log('Request [swagger] Body (req.swagger.params.user.value): ' + JSON.stringify(req.swagger.params.user.value));
+
+  var user = req.swagger.params.user.value;
+  if(user.username[0] === 't') {
+    //[201] success
+    res.status(201).send({
+      username: user.username,
+      message: 'User successfully registered.'
+    });
+  } else if(user.username[0] === 'k') {
+    //[422] username not unique
+  } else if(!user.email.contains('@deluxe.com')){
+    //[422] email not unique
+  } else if(user.password.replace(/\s/g, '').length === 0) {
+    //[422] password is empty string
+  } else {
+    //[500] default server error
+  }
 }
